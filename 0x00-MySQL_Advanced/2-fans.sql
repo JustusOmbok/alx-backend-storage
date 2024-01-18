bbcb2c323702aa3f7759a3c99c07dd0b7db96b69
JustusOmbok <ombokjustus@gmail.com>
2024-01-18 14:36:15 +0300

    ranks banda

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- Rank country origins of bands by the number of non-unique fans
+-- Assumes the metal_bands table is already imported
+
+SELECT origin, COUNT(*) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
