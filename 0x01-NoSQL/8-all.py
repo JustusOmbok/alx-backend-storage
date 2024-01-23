d095744bbe18647eeb2807d82a0b7498e9308930
root <JustusOmbok>
2024-01-23 05:55:15 -0800

    gets all docs in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Module for 8-all """
+from pymongo.collection import Collection
+from typing import List
+
+
+def list_all(mongo_collection: Collection) -> List[dict]:
+    """
+    Lists all documents in a MongoDB collection.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The pymongo collection object.
+
+    Returns:
+        List[dict]: A list containing all documents in the collection.
+    """
+    documents = list(mongo_collection.find())
+    return documents
+
