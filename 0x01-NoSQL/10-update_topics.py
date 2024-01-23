8b5dc3f334abc07f85b68a56fb2c4e8449dfbda3
root <JustusOmbok>
2024-01-23 06:34:19 -0800

    updates a doc

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,10 +7,10 @@ def update_topics(mongo_collection, name, topics):
     '''Changes all topics of a school document based on the name.
 
     Args:
-        mongo_collection (pymongo.collection.Collection): The pymongo collection object.
+        mongo_collection: The pymongo collection object.
         name (str): The school name to update.
         topics (list of str): The list of topics approached in the school.
     '''
     query = {"name": name}
     update = {"$set": {"topics": topics}}
-    mongo_collection.update_one(query, update)
+    mongo_collection.update_many(query, update)
