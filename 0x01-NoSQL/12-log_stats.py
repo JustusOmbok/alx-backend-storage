20f1c5d632ad34e53558dc2629f0e574af226faf
root <JustusOmbok>
2024-01-23 06:54:39 -0800

    nginx logs

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -21,3 +21,9 @@ def log_stats(mongo_collection):
 
     status_check_count = mongo_collection.count_documents({"method": "GET", "path": "/status"})
     print("{} status check".format(status_check_count))
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+
+    log_stats(logs_collection)
