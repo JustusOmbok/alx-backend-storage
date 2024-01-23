34cbc3a6fb306274c198bdce60421b43bdd91a0c
root <JustusOmbok>
2024-01-23 06:30:30 -0800

    updates a doc

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Task 10's module.
+'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''Changes all topics of a school document based on the name.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The pymongo collection object.
+        name (str): The school name to update.
+        topics (list of str): The list of topics approached in the school.
+    '''
+    query = {"name": name}
+    update = {"$set": {"topics": topics}}
+    mongo_collection.update_one(query, update)
